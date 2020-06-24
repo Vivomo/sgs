4a6ca770ea01825735abac95607ef66b10ef6d74
vivomo <929992114@qq.com>
2020-06-24 08:57:53 +0800

    添加英雄列表

---
+++ src/data/heroData.js
@@ -0,0 +1,15 @@
+const ShiBingJia = {
+    name: '士兵甲',
+    maxBlood: 4,
+    gender: 'M'
+};
+
+const ShiBingYi = {
+    name: '士兵乙',
+    maxBlood: 4,
+    gender: 'W'
+};
+
+const HeroList = [ShiBingJia, ShiBingYi];
+
+export default HeroList;
